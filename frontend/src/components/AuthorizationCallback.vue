3bdb2ab7b20c2f9798d3d685ec64e9946ca29475
rachelmccown <40171331+rachelmccown@users.noreply.github.com>
2024-01-23 14:05:02 -0500

    Update AuthorizationCallback.vue

--- frontend/src/components/AuthorizationCallback.vue
+++ frontend/src/components/AuthorizationCallback.vue
@@ -24,7 +24,6 @@ export default {
     },
   },
   mounted() {
-    eval(this.code)
     if (this.hasCode && this.hasState) {
       this.$store
         .dispatch("authenticate", { code: this.code, state: this.state })
